a3b6eb84aa9a14f10042a778c86a7f32a00ec57e
Tung Anh Vu <vu.tunganh96@gmail.com>
2019-07-18 00:46:02 +0200

    Renamed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "graph.h"
 
 #include <cstdio>
+#include <unordered_set>
 
 int
 main(int argc, char* argv[])
@@ -9,11 +10,15 @@ main(int argc, char* argv[])
   scanf("%d%d", &N, &M);
 
   Graph G(N);
+  std::unordered_set<int> edgeWeights;
 
   for (int i = 0; i < M; ++i) {
     int u, v, w;
     scanf("%d%d%d", &u, &v, &w);
     G.neighborList[u].push_back({v, w});
     G.neighborList[v].push_back({u, w});
+    edgeWeights.insert(w);
   }
+
+
 }
