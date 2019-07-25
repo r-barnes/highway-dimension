bd6726a80a016f11123cfd7a19556f08d62705ef
Tung Anh Vu <vu.tunganh96@gmail.com>
2019-07-25 11:23:23 +0200

    Better output for various plots

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,39 @@
 
 #include <cstdio>
 
+enum class EXIT_CODES {
+  OK, INVALID_INPUT, COULD_NOT_OPEN_FILE
+};
+
 int
 main(int argc, char* argv[])
 {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s graph.in\n", argv[0]);
-    return 1;
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s graph.in output.out\n", argv[0]);
+    return EXIT_CODES::INVALID_INPUT;
   }
-  // FIXME: read input properly
-  freopen(argv[1], "r", stdin);
+  FILE* inputFile = fopen(argv[1], "r");
+  if (!inputFile) {
+    fputs("Could not open input graph.\n", stderr);
+    return EXIT_CODES::COULD_NOT_OPEN_FILE;
+  }
+  FILE* outputFile = fopen(argv[2], "w+");
+  if (!outputFile) {
+    fputs("Could not open output file.\n", stderr);
+    return EXIT_CODES::COULD_NOT_OPEN_FILE;
+  }
+
   int N, M;
-  scanf("%d%d", &N, &M);
+  fscanf(inputFile, "%d%d", &N, &M);
 
   Graph<WeightedEdge> G(N);
-
   #ifdef DEBUG
   fputs("Processing graph.....", stderr);
   #endif
   for (int i = 0; i < M; ++i) {
     int u, v;
     int64_t w;
-    scanf("%d%d%" SCNd64, &u, &v, &w);
+    fscanf(inputFile, "%d%d%" SCNd64, &u, &v, &w);
     G.addEdge(u, {v, w});
     G.addEdge(v, {u, w});
   }
@@ -31,5 +43,7 @@ main(int argc, char* argv[])
   fputs("done\n", stderr);
   #endif
 
-  printf("%d\n", approximateHd(G));
+  fprintf(outputFile, "%d %d %d\n", N, M, approximateHd(G));
+  fclose(inputFile);
+  fclose(outputFile);
 }
